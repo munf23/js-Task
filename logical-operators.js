30e7af5729725e5fb6bf48b5fb29dfd47bf557f6
zehraaMA <zahraa@thecodepeople.io>
2023-04-02 03:22:12 +0300

    reorganize the structure

--- logical-operators.js
+++ logical-operators.js
@@ -1,35 +1,51 @@
 //Logical and Comparison Operators
 
-// TASK 1:
-/* In this task, you will be given a set of expressions and your job is to determine the result of each expression.*/
+/*******************************************************************************
+TASK 1:
+     In this task, you will be presented with a series of expressions that you need to evaluate and determine if the result is true or false.
+ // Have fun! // 😃
+********************************************************************************/
 
-// - const exp1 = 10 >= 10
-// - const exp2 = "dog" == "dog"
-// - const exp3 = true != false
-// - const exp4 = "10" === 10
-// - const exp5= 5 > 4
-// - const exp6= null == undefined
-// - const exp7= "true" == true
-// - const exp8= "false" == false
-// - const exp9= NaN === NaN
-// - const exp10= !false || false
-// - const exp11 = false && !false
-// - const exp12 = "apple" > "pineapple"
-// - const exp13 = "2" > "12"
-// - const exp14 = undefined == null
-// - const exp15 = undefined === nul
+const exp1 = 10 >= 10;
+const exp2 = "dog" == "dog";
+const exp3 = true != false;
+const exp4 = "10" === 10;
+const exp5 = 5 > 4;
+const exp6 = null == undefined;
+const exp7 = "true" == true;
+const exp8 = "false" == false;
+const exp9 = NaN === NaN;
+const exp10 = !false || false;
+const exp11 = false && !false;
+const exp12 = "apple" > "pineapple";
+const exp13 = "2" > "12";
+const exp14 = undefined == null;
+const exp15 = undefined === null;
 
-/************************************************************* */
-// Task 2:
-/* In the following tasks, you will practice using logical operators in JavaScript. */
+/*******************************************************************************
+Task 2:
+    In the following tasks, you will practice using logical operators in JavaScript, in addition to some strings methods.
+// Have fun! // 😃 
+********************************************************************************/
 
-// - Check if a number is between 10 and 20 (inclusive) using the logical AND operator. Log the result to the console.
-// - Check if a number is either less than 5 or greater than 50 using the logical OR operator. Log the result to the console.
-// - Check if a string is either "apple" or "orange" using the logical OR operator. Log the result to the console.
-// - Check if a boolean value is true using the logical NOT operator. Log the result to the console.
-// - Check if a number is even and greater than 10 using the logical AND operator. Log the result to the console.
-// - Check if a number is divisible by both 3 and 5 using the logical AND operator. Log the result to the console.
-// - Check if a string contains the letter "e" using the logical AND operator. Log the result to the console.
-// - Check if a string starts with "Hello" using the logical AND operator. Log the result to the console.
-// - Check if a string ends with "world" using the logical AND operator. Log the result to the console.
-// - Check if a number is either negative or odd using the logical OR operator. Log the result to the console.
+// - Check if num is between 10 and 20 (inclusive) using the logical AND operator. Log the result to the console.
+
+// - Check if num is either less than 5 or greater than 50 using the logical OR operator. Log the result to the console.
+
+// - Check if str is either "apple" or "orange" using the logical OR operator. Log the result to the console.
+
+// - Check if isHappy value is true using the logical NOT operator. Log the result to the console.
+
+// - Check if num is even and greater than 10 using the logical AND operator. Log the result to the console.
+
+// - Check if num is divisible by both 3 and 5 using the logical OR operator. Log the result to the console.
+
+// - Check if str contains the letter "e". Log the result to the console.
+
+// - Check if str starts with "Hakuna". Log the result to the console.
+
+// - Check if str ends with "a". Log the result to the console.
+
+// - Check if num is either negative or odd using the logical OR operator. Log the result to the console.
+
+// - Check if the length of str is greater than num or equal to 40 using logical OR operator. Log the result to the console.
