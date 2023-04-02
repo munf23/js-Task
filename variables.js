5b636e92fc6be80cc0870b352ddbaa7dcd8b0e61
zehraaMA <zahraa@thecodepeople.io>
2023-04-02 03:23:11 +0300

    reorganize the structure

--- variables.js
+++ variables.js
@@ -1,37 +1,40 @@
+/*******************************************************************************
+ This task will provide you with an opportunity to practice creating and manipulating variables,
+ as well as using string concatenation and getting familiar with different datatypes.
+ // Have fun! //😃
+********************************************************************************/
+
 // Task 1 (Variables):
+// - Create three variables named 'personName', 'age', and 'isHappy', where:
+//  'personName' should hold a string value
+//  'age' should hold a numeric value
+//  'isHappy' should hold a boolean value.
 
-// - Create three variables named 'name', 'age', and 'isHappy', where 'name' should hold a string value, 'age' should hold a numeric value, and 'isHappy' should hold a boolean value. Use console.log() to output the value of each variable to the console.
+// - Use console.log() to output the value of each variable.
 
-// - Create a new variable named 'nickName' and assign the value of the 'name' variable to the 'nickName' variable. Use console.log o output the value of 'nickName'
+/*******************************************************************************/
+// Task 2 (Reassigning variables):
+
+// - Create a new variable named 'nickName' and assign the value of the 'personName' variable to the 'nickName' variable.
+// - Use console.log o output the value of 'nickName'
+
+/*******************************************************************************/
+// Task 3 (Naming variables):
 
 // - Create a variable with the title of your favorite movie. How would you name such a variable?
 
 // - Declare a variable that stores the age of a user. What name would you choose for this variable?
 
-/************************************************************************** */
-// Task 2 (String Concatenation):
+/*******************************************************************************/
+// Task 4 (String Concatenation):
 
 // Build upon the previous task by completing the following steps:
+// 1. Declare a new variable called 'msg'.
 
-// 1. Add a new variable called 'msg' to the existing code.
+// 2. Use the `prompt()` function to ask the user to enter a message for the person you just created. Then assign the value to the `msg` variable.
 
-// 2. Use the `prompt()` function to ask the user to enter a message for the person you just created.
-
-// 3. Using string concatenation, create a message that starts with "Hello, {name}".
-
-// 4. If 'isHappy' is true, add the question "Why are you happy?" to the message.
-
-// 5. If 'isHappy' is false, add the response "Good for you. There's nothing to be happy about." to the message
-
-/************************************************************************** */
-// Task 3 (String and Number methods):
-
-// 6. Use the `.length` property to find the length of the message and store it in a new variable called 'msgLength'.
-
-// 7. Use the `.toUpperCase()` method to convert the value of the 'name' variable to uppercase, and store the result in a new variable called 'nameUpper'.
-
-// 8. Print the final message to the console, including the name in uppercase and the length of the message.
-
-// 9. Add a new variable called 'gpa' to hold the student's GPA in college.
-
-// 10. Use the `.toFixed()` method to keep only one decimal place in the value of the 'gpa' variable.
+// 3. Use string concatenation to create a message
+//   - If 'isHappy' is true, add the question "And btw, Why are you happy?" to the message.
+//   - If 'isHappy' is false, add the response "And btw, Good for you. There's nothing to be happy about." to the message
+//   - Assign the completed message to a variable called finalMsg
+//   - Print the final message to the console, including the personName in uppercase in this format `Dear personName_VALUE, here's your message: finalMsg_VALUE.`.
