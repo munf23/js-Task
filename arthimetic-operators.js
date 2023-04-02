31a3069451540cbe9c0cd3c88191cbebccae5844
zehraaMA <zahraa@thecodepeople.io>
2023-04-02 03:20:51 +0300

    reorganize the structure

--- arthimetic-operators.js
+++ arthimetic-operators.js
@@ -1,20 +1,38 @@
-// Arithmetic Operators
+/*******************************************************************************
+ Arithmetic Operators
+ In these exercises, you will be practicing some basic arithmetic and variable manipulation in JavaScript.
+ You will be using the console to log your results and get familiar with different data types in JavaScript.
+ Have fun! 😃
+// Note: Please make sure to limit the number of decimal places to three for any value that may have more than two decimal places by using the .toFixed() method.
+*******************************************************************************/
 
-// Task 1:
-/* In these exercises, you will be practicing some basic arithmetic and variable manipulation in JavaScript. You will be using the console to log your results and get familiar with different data types in JavaScript. These exercises do not require any prior knowledge of programming, but they will help you to become comfortable with some of the core concepts in JavaScript. Have fun!*/
-//1. Subtract two numbers and log the result to the console.
-//2. Find the product of four consecutive odd numbers and log the result to the console.
-//3. Calculate the volume of a rectangular prism with dimensions 3x5x7 and log the result to the console.
-//4. Declare a variable price and assign it the value 9.99. Declare another variable discount and assign it the value 0.20. Calculate the discounted price and log the result to the console.
-//5. Declare a variable message and assign it a string value. Use string interpolation to add more text to the message and log the result to the console.
-//6. Divide two numbers and round the result to the nearest integer before logging it to the console.
+/*******************************************************************************
+ // Task 1:
+    In this task, you will use JavaScript to perform some basic math operations. Follow the steps below to complete the task.
+ *******************************************************************************/
+//1. Define variables a, b, c, d, and e.
 
-// Task 2:
-/* In this task, you will use JavaScript to perform some basic math operations. Follow the steps below to complete the task.*/
-//1. Define variables a, b, c, d, and e using JavaScript.
 //2. Set the value of variable a to the result of adding 4 to 6.
+
 //3. Set the value of variable b to the result of multiplying 10 by 5.
+
 //4. Set the value of variable c to the remainder of dividing 17 by 3.
+
 //5. Set the value of variable d to the result of subtracting variable a from variable b.
+
 //6. Set the value of variable e to the sum of variables a, b, c, and d.
-//7. Use console.log() to print the value of variable e to the console.
+
+//7. Use console.log() to print the value of variable e to the console. (it should result 105.667)
+
+/*******************************************************************************/
+// Task 2:
+
+//1. Subtract two numbers and log the result to the console.
+
+//2. Multiply together the values of four odd numbers that come one after the other, and then output the final result to the console.
+
+//3. Calculate the volume of a rectangular prism with dimensions 3x5x7 and log the result to the console.
+
+//4. Declare a variable price and assign it the value 9.99. Declare another variable discount and assign it the value 0.20. Calculate the discounted price and log the result to the console.
+
+//5. Divide two numbers and round the result to the nearest integer before logging it to the console.
