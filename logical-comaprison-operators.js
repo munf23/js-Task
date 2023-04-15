0e70a684508bf5336694da8c25286335460e2697
zehraaMA <zahraa@thecodepeople.io>
2023-04-15 21:16:05 +0300

    reorganize the file structure

--- logical-comaprison-operators.js
+++ logical-comaprison-operators.js
@@ -7,19 +7,33 @@ TASK 1:
 ********************************************************************************/
 
 const exp1 = 10 >= 10; // TODO: ADD YOUR EVALUATION HERE -->
+
 const exp2 = "dog" == "dog"; // TODO: ADD YOUR EVALUATION HERE -->
+
 const exp3 = true != false; // TODO: ADD YOUR EVALUATION HERE -->
+
 const exp4 = "10" === 10; // TODO: ADD YOUR EVALUATION HERE -->
+
 const exp5 = 5 > 4; // TODO: ADD YOUR EVALUATION HERE -->
+
 const exp6 = null == undefined; // TODO: ADD YOUR EVALUATION HERE -->
+
 const exp7 = "true" == true; // TODO: ADD YOUR EVALUATION HERE -->
+
 const exp8 = "false" == false; // TODO: ADD YOUR EVALUATION HERE -->
+
 const exp9 = NaN === NaN; // TODO: ADD YOUR EVALUATION HERE -->
+
 const exp10 = !false || false; // TODO: ADD YOUR EVALUATION HERE -->
+
 const exp11 = false && !false; // TODO: ADD YOUR EVALUATION HERE -->
+
 const exp12 = "apple" > "pineapple"; // TODO: ADD YOUR EVALUATION HERE -->
+
 const exp13 = "2" > "12"; // TODO: ADD YOUR EVALUATION HERE -->
+
 const exp14 = undefined == null; // TODO: ADD YOUR EVALUATION HERE -->
+
 const exp15 = undefined === null; // TODO: ADD YOUR EVALUATION HERE -->
 
 /*******************************************************************************
@@ -27,36 +41,39 @@ Task 2:
     In the following tasks, you will practice using logical operators in JavaScript, in addition to some strings methods.
 // Have fun! // 😃 
 ********************************************************************************/
+const num = 15;
+const str = "Hakuna Matata";
+const isHappy = false;
 
 // - Check if num is between 10 and 20 (inclusive) using the logical AND operator. Log the result to the console.
-// TODO: ADD YOUR CODE HERE
+// TODO: ADD YOUR CODE BELOW
 
 // - Check if num is either less than 5 or greater than 50 using the logical OR operator. Log the result to the console.
-// TODO: ADD YOUR CODE HERE
+// TODO: ADD YOUR CODE BELOW
 
 // - Check if str is either "apple" or "orange" using the logical OR operator. Log the result to the console.
-// TODO: ADD YOUR CODE HERE
+// TODO: ADD YOUR CODE BELOW
 
 // - Check if isHappy value is true using the logical NOT operator. Log the result to the console.
-// TODO: ADD YOUR CODE HERE
+// TODO: ADD YOUR CODE BELOW
 
 // - Check if num is even and greater than 10 using the logical AND operator. Log the result to the console.
-// TODO: ADD YOUR CODE HERE
+// TODO: ADD YOUR CODE BELOW
 
 // - Check if num is divisible by both 3 and 5 using the logical OR operator. Log the result to the console.
-// TODO: ADD YOUR CODE HERE
+// TODO: ADD YOUR CODE BELOW
 
 // - Check if str contains the letter "e". Log the result to the console.
-// TODO: ADD YOUR CODE HERE
+// TODO: ADD YOUR CODE BELOW
 
 // - Check if str starts with "Hakuna". Log the result to the console.
-// TODO: ADD YOUR CODE HERE
+// TODO: ADD YOUR CODE BELOW
 
 // - Check if str ends with "a". Log the result to the console.
-// TODO: ADD YOUR CODE HERE
+// TODO: ADD YOUR CODE BELOW
 
 // - Check if num is either negative or odd using the logical OR operator. Log the result to the console.
-// TODO: ADD YOUR CODE HERE
+// TODO: ADD YOUR CODE BELOW
 
 // - Check if the length of str is greater than num or equal to 40 using logical OR operator. Log the result to the console.
-// TODO: ADD YOUR CODE HERE
+// TODO: ADD YOUR CODE BELOW
