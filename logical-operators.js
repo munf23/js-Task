4009c01d8a95fd3d2bfbbddf16d376d69f4bd251
zehraaMA <zahraa@thecodepeople.io>
2023-04-15 20:55:59 +0300

    Refactor the tasks instructions

--- logical-operators.js
+++ logical-operators.js
@@ -6,21 +6,21 @@ TASK 1:
  // Have fun! // 😃
 ********************************************************************************/
 
-const exp1 = 10 >= 10;
-const exp2 = "dog" == "dog";
-const exp3 = true != false;
-const exp4 = "10" === 10;
-const exp5 = 5 > 4;
-const exp6 = null == undefined;
-const exp7 = "true" == true;
-const exp8 = "false" == false;
-const exp9 = NaN === NaN;
-const exp10 = !false || false;
-const exp11 = false && !false;
-const exp12 = "apple" > "pineapple";
-const exp13 = "2" > "12";
-const exp14 = undefined == null;
-const exp15 = undefined === null;
+const exp1 = 10 >= 10; // TODO: ADD YOUR EVALUATION HERE -->
+const exp2 = "dog" == "dog"; // TODO: ADD YOUR EVALUATION HERE -->
+const exp3 = true != false; // TODO: ADD YOUR EVALUATION HERE -->
+const exp4 = "10" === 10; // TODO: ADD YOUR EVALUATION HERE -->
+const exp5 = 5 > 4; // TODO: ADD YOUR EVALUATION HERE -->
+const exp6 = null == undefined; // TODO: ADD YOUR EVALUATION HERE -->
+const exp7 = "true" == true; // TODO: ADD YOUR EVALUATION HERE -->
+const exp8 = "false" == false; // TODO: ADD YOUR EVALUATION HERE -->
+const exp9 = NaN === NaN; // TODO: ADD YOUR EVALUATION HERE -->
+const exp10 = !false || false; // TODO: ADD YOUR EVALUATION HERE -->
+const exp11 = false && !false; // TODO: ADD YOUR EVALUATION HERE -->
+const exp12 = "apple" > "pineapple"; // TODO: ADD YOUR EVALUATION HERE -->
+const exp13 = "2" > "12"; // TODO: ADD YOUR EVALUATION HERE -->
+const exp14 = undefined == null; // TODO: ADD YOUR EVALUATION HERE -->
+const exp15 = undefined === null; // TODO: ADD YOUR EVALUATION HERE -->
 
 /*******************************************************************************
 Task 2:
@@ -29,23 +29,34 @@ Task 2:
 ********************************************************************************/
 
 // - Check if num is between 10 and 20 (inclusive) using the logical AND operator. Log the result to the console.
+// TODO: ADD YOUR CODE HERE
 
 // - Check if num is either less than 5 or greater than 50 using the logical OR operator. Log the result to the console.
+// TODO: ADD YOUR CODE HERE
 
 // - Check if str is either "apple" or "orange" using the logical OR operator. Log the result to the console.
+// TODO: ADD YOUR CODE HERE
 
 // - Check if isHappy value is true using the logical NOT operator. Log the result to the console.
+// TODO: ADD YOUR CODE HERE
 
 // - Check if num is even and greater than 10 using the logical AND operator. Log the result to the console.
+// TODO: ADD YOUR CODE HERE
 
 // - Check if num is divisible by both 3 and 5 using the logical OR operator. Log the result to the console.
+// TODO: ADD YOUR CODE HERE
 
 // - Check if str contains the letter "e". Log the result to the console.
+// TODO: ADD YOUR CODE HERE
 
 // - Check if str starts with "Hakuna". Log the result to the console.
+// TODO: ADD YOUR CODE HERE
 
 // - Check if str ends with "a". Log the result to the console.
+// TODO: ADD YOUR CODE HERE
 
 // - Check if num is either negative or odd using the logical OR operator. Log the result to the console.
+// TODO: ADD YOUR CODE HERE
 
 // - Check if the length of str is greater than num or equal to 40 using logical OR operator. Log the result to the console.
+// TODO: ADD YOUR CODE HERE
--- variables.js
+++ variables.js
@@ -1,40 +1,49 @@
 /*******************************************************************************
- This task will provide you with an opportunity to practice creating and manipulating variables,
+ These tasks will provide you with an opportunity to practice creating and manipulating variables,
  as well as using string concatenation and getting familiar with different datatypes.
  // Have fun! //😃
 ********************************************************************************/
 
-// Task 1 (Variables):
-// - Create three variables named 'personName', 'age', and 'isHappy', where:
-//  'personName' should hold a string value
-//  'age' should hold a numeric value
-//  'isHappy' should hold a boolean value.
+/*******************************************************************************
+ Task 1 (Variables):
+ 
+ Steps:
+    1. Create three variables named 'personName', 'age', and 'isHappy', where:
+        - 'personName' should hold a string value
+        - 'age' should hold a numeric value
+        - 'isHappy' should hold a boolean value.
+    2. Use console.log() to output the value of each variable.
+ ********************************************************************************/
+// TODO: ADD YOUR CODE HERE
 
-// - Use console.log() to output the value of each variable.
+/*******************************************************************************
+ Task 2 (Reassigning variables):
+ 
+ Steps:
+    1. Create a new variable named 'nickName' and assign the value of the 'personName' variable to the 'nickName' variable.
+    2. Use console.log o output the value of 'nickName'
+ *******************************************************************************/
+// TODO: ADD YOUR CODE HERE
 
-/*******************************************************************************/
-// Task 2 (Reassigning variables):
+/*******************************************************************************
+ Task 3 (Naming variables):
+ 
+ Steps:
+    1. Create a variable with the title of your favorite movie. How would you name such a variable?
+    2. Declare a variable that stores the age of a user. What name would you choose for this variable?
+*******************************************************************************/
+// TODO: ADD YOUR CODE HERE
 
-// - Create a new variable named 'nickName' and assign the value of the 'personName' variable to the 'nickName' variable.
-// - Use console.log o output the value of 'nickName'
+/*******************************************************************************
+Task 4 (String Concatenation):
+    Build upon the previous task by completing the following steps:
 
-/*******************************************************************************/
-// Task 3 (Naming variables):
-
-// - Create a variable with the title of your favorite movie. How would you name such a variable?
-
-// - Declare a variable that stores the age of a user. What name would you choose for this variable?
-
-/*******************************************************************************/
-// Task 4 (String Concatenation):
-
-// Build upon the previous task by completing the following steps:
-// 1. Declare a new variable called 'msg'.
-
-// 2. Use the `prompt()` function to ask the user to enter a message for the person you just created. Then assign the value to the `msg` variable.
-
-// 3. Use string concatenation to create a message
-//   - If 'isHappy' is true, add the question "And btw, Why are you happy?" to the message.
-//   - If 'isHappy' is false, add the response "And btw, Good for you. There's nothing to be happy about." to the message
-//   - Assign the completed message to a variable called finalMsg
-//   - Print the final message to the console, including the personName in uppercase in this format `Dear personName_VALUE, here's your message: finalMsg_VALUE.`.
+Steps:
+    1. Declare a new variable called 'msg'.
+    2. Use the `prompt()` function to ask the user to enter a message for the person you just created. Then assign the value to the `msg` variable.
+    3. Use string concatenation to create a message
+        - If 'isHappy' is true, add the question "And btw, Why are you happy?" to the message.
+        - If 'isHappy' is false, add the response "And btw, Good for you. There's nothing to be happy about." to the message
+        - Assign the completed message to a variable called finalMsg
+        - Print the final message to the console, including the personName in uppercase in this format `Dear personName_VALUE, here's your message: finalMsg_VALUE.`.
+*******************************************************************************/
